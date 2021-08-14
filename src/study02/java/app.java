f12db3978d0b9e37b0085724c9c84938e4f59e6e
JungKyuHyun <ajdkfl6445@gmail.com>
2021-08-15 00:25:40 +0900

    build(java): modify indent and delete unused import

--- src/study02/java/app.java
+++ src/study02/java/app.java
@@ -1,12 +1,7 @@
 package study02.java;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class app {
-  public static void main(String[] args) {
-    System.out.println("인텔리제이로 바꿈.. 적응중?");
-  }
+    public static void main(String[] args) {
+        System.out.println("인텔리제이로 바꿈.. 적응중?");
+    }
 }
